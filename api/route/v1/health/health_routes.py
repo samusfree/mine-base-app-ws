d3b4affc16d06dafbbca722bcdc4f4208411084e
Samuel Gonzales <sagonzales89@gmail.com>
2024-10-11 17:07:17 -0500

    add doc swagger

--- api/route/v1/health/health_routes.py
+++ api/route/v1/health/health_routes.py
@@ -5,4 +5,24 @@ health_blueprint_v1 = Blueprint("health_bp", __name__)
 
 @health_blueprint_v1.route("/health-check", methods=["GET"])
 def health():
+    """
+    Health check endpoint
+    ---
+    get:
+        description: Health check endpoint
+        responses:
+            200:
+                description: Health check response
+                content:
+                    application/json:
+                        schema:
+                            type: object
+                            properties:
+                                status:
+                                    type: string
+                            example: 
+                                status : UP
+        tags:
+            - health
+        """
     return jsonify({"status": "UP"}), 200
